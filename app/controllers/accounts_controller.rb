96619b98e9061bb5b183dc076b5a2eda5468052b
Shinji Kakimoto <skakimoto@cuon.co.jp>
2016-11-27 17:05:17 +0900

    Added subdomains to accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -9,12 +9,12 @@ class AccountsController < ApplicationController
     # HACK: ユーザクラスでログインしている。
     sign_in(account.owner)
     flash[:notice] = "Your account has been created."
-    redirect_to root_url
+    redirect_to root_url(subdomain: account.subdomain)
   end
 
 private
   def account_params
-    params.require(:account).permit(:name,
+    params.require(:account).permit(:name, :subdomain,
                                     { owner_attributes: [ :email, :password, :password_confirmation ] }
                                    )
   end
