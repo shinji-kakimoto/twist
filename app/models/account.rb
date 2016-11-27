e895d7ba9dde830c225ed50acd7053b091f74dd0
Shinji Kakimoto <skakimoto@cuon.co.jp>
2016-11-27 17:22:47 +0900

    Added uniqueness validation for subdomain

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,5 @@
 class Account < ActiveRecord::Base
+  validates :subdomain, presence: true, uniqueness: true
   belongs_to :owner, class_name: "User"
   #TODO 子から親を作るってこと？
   accepts_nested_attributes_for :owner
