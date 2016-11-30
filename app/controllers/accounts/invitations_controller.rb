317092d56984b91a72c8000e681445d8a8038ebf
Shinji Kakimoto <skakimoto@cuon.co.jp>
2016-11-30 21:10:20 +0900

    Users can now accept invitations to an account

--- app/controllers/accounts/invitations_controller.rb
+++ app/controllers/accounts/invitations_controller.rb
@@ -1,7 +1,10 @@
 #TODO モジュールとクラスはなにが違うのか...?どこかで再利用するのか？
 module Accounts
   class InvitationsController < Accounts::BaseController
-    before_action :authorize_owner!
+    # ユーザのログイン
+    skip_before_action :authenticate_user!, only: [:accept, :accepted]
+    # 自前のower権限チェック
+    before_action :authorize_owner!, except: [:accept, :accepted]
 
     def new
       @invitation = Invitation.new
@@ -13,6 +16,26 @@ module Accounts
       InvitationMailer.invite(@invitation).deliver_later
       flash[:notice] = "#{@invitation.email} has been invited."
       redirect_to root_url
+    end
+
+    # リンク先でここにくる
+    def accept
+      @invitation = Invitation.find(params[:id])
+    end
+
+    def accepted
+      @invitation = Invitation.find(params[:id])
+      user_params = params[:user].permit(
+        :email,
+        :password,
+        :password_confirmation
+      )
+
+      user = User.create!(user_params)
+      current_account.users << user
+      sign_in(user)
+      flash[:notice] = "You have joined the #{current_account.name} account."
+      redirect_to root_url(subdomain: current_account.subdomain)
     end
 
     private
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   def after_sign_out_path_for(resource)
     signed_out_path
   end
